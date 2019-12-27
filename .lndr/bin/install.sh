a0afc3dc5bc5e8509b9b8fb1560b07d1cc8dbab7
Matt Wiley <wileymab@gmail.com>
2019-12-27 16:40:14 -0500

    Extended install script to unzip download properly. Added static download to repo.

--- .lndr/bin/install.sh
+++ .lndr/bin/install.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
 # curl down the zip of the ./bin dir
-# unzip it at ~/.lndr
-#link ~/.lndr/bin/lndr.sh /usr/local/bin/lndr
-# set LANDER_HOME=~/.lndr
+
+
+echo -n "Unpacking LZ files ... "
+unzip lz.zip -d ~/.lndr
+echo "done."
+
+echo -n "Installing executable ... "
+link ~/.lndr/bin/lndr.sh /usr/local/bin/lndr
+echo "done."
 
 echo -n "Initializing zone registry ... "
-sleep 1 #remove
 mkdir -p ${LANDER_HOME}/zones
 echo "done."
-sleep 1 #remove
 
-
-echo -n "Adding zone loader to .bashrc ... "
-sleep 1 #remove
+echo -n "Initalizing zone loader in .bashrc ... "
 echo "" >> ~/.bashrc
 echo "# LZ Loader" >> ~/.bashrc
+echo "export LANDER_HOME=~/.lndr" >> ~/.bashrc
 echo "source ${LANDER_HOME}/bin/bashrc-hook.sh" >> ~/.bashrc
 echo "done."
