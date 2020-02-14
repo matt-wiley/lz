0c6a96d4a6c253de6ef9f183e14ca8f6c3c3b984
Matt Wiley <wileymab@gmail.com>
2020-02-13 21:28:50 -0500

    Updated install script to be a bit smarter about bash alterations and initialization.

--- .lndr/lib/install.sh
+++ .lndr/lib/install.sh
@@ -39,14 +39,44 @@ echo "done."
 echo ""
 echo -n "Adding executable to PATH ... "
 ln -sf ${LANDER_HOME}/lib/lndr.sh ${LANDER_HOME}/bin/lndr
-echo "# LZ Path Amendment" >> ~/.bashrc
-echo "export PATH=\"${LANDER_HOME}/bin:$PATH\"" >> ~/.bashrc
-echo "done."
+
+function split {
+    IFS="${1}"
+    read -ra ADDR <<< "$@"
+    for i in "${ADDR[@]}"; do
+        echo $i
+    done
+}
+paths=($(split ':' "${PATH}"))
+
+# echo "${PATH}"
+# echo "${paths[@]}"
+
+found="false"
+for path in ${paths[@]}; do
+    # echo "${path}"
+    if [[ "${path}" == "${LANDER_HOME}/bin" ]]; then
+        found="true"
+    fi
+done
+# echo "${found}" 
+if [[ "${found}" == "false" ]]; then
+    echo "# LZ Path Amendment" >> ~/.bashrc
+    echo "export PATH=\"${LANDER_HOME}/bin:$PATH\"" >> ~/.bashrc
+    echo "done."
+else
+    echo "done. (LZ found in PATH.)"
+fi
 
 echo ""
 echo -n "Initalizing zone loader in .bashrc ... "
-echo "" >> ~/.bashrc
-echo "# LZ Loader" >> ~/.bashrc
-echo "export LANDER_HOME=~/.lndr" >> ~/.bashrc
-echo "source ${LANDER_HOME}/lib/bashrc-hook.sh" >> ~/.bashrc
-echo "done."
+
+if [[ -n "${LANDER_HOME}" ]]; then 
+    echo "done. (LANDER_HOME already set.)"
+else 
+    echo "" >> ~/.bashrc
+    echo "# LZ Loader" >> ~/.bashrc
+    echo "export LANDER_HOME=~/.lndr" >> ~/.bashrc
+    echo "source ${LANDER_HOME}/lib/bashrc-hook.sh" >> ~/.bashrc
+    echo "done."
+fi
