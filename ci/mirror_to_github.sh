aeeb62a77bd95955c8ae6270732c360062fd5ec2
Matt Wiley <mattwiley+gitlab@fastmail.com>
2021-04-16 16:55:29 -0400

    Trying the manual repo mirroring again. (6)

--- ci/mirror_to_github.sh
+++ ci/mirror_to_github.sh
@@ -7,6 +7,7 @@ function main {
         mkdir -p .tmp
         cd .tmp
         git clone --bare "https://gitlab.com/mattwiley/lz.git"
+        git co main
         git push --mirror "https://${user}:${pass}@github.com/matt-wiley/lz"
         cd ..
         rm -rf .tmp
